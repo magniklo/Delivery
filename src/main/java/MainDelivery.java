00904223bcc77fc146a637ef89c380953cd8c893
magniklo <magniklom@gmail.com>
2025-11-22 10:18:14 +0300

    Update MainDelivery.java

--- src/main/java/MainDelivery.java
+++ src/main/java/MainDelivery.java
@@ -2,6 +2,6 @@ public class MainDelivery {
     public static void main(String[] args) {
         Saller saller = new Saller("Martynov Albert", 214011);
         saller.showInformation();
-
+    int u = 0;
     }
 }
